1295eea412c3787bd0d3a10386b90c53427f6647
savioomio <saviopessoaafonso@gmail.com>
2023-05-02 09:04:11 -0300

    Create index.php

---
+++ Atividade-Array/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>arrays-Exercicios</h1>
+
+    <?php
+
+    echo '<h2> Exercicios de arrays - nome do aluno</h2>';
+
+    echo '<hr>';
+
+    $notas_alunos = array(
+        'PSW1' => array(
+            'avaliação' => 6,
+            'teste' => 7
+        ),
+        'BD' => array(
+            'avaliação' => 5,
+            'teste' => 9
+        ),
+        'Redes' => array(
+            'avaliação' => 10,
+            'teste' => 10
+        ),
+        'ASW1' => array(
+            'avaliação' => 4,
+            'teste' => 2
+        ),
+        'PI1' => array(
+            'avaliação' => 8,
+            'teste' => 9
+        )
+    );
+
+    echo '<hr>';
+
+    ?>
+
+</body>
+
+</html>
